6dde5550ac3faec23a382770cd5a9f365b11ede8
shabab <shababuddin46@gmail.com>
2025-07-04 10:53:47 +0530

    Updated employees endpoint

--- get_employees.php
+++ get_employees.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, DELETE, PUT, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Credentials: true");
 
 // Include the database connection
