1e20c56d2bab4959ee0d3bcec7d355541203b41f
dev.vishwakarma17 <dev.vishwakarma17@gmail.com>
2025-11-13 15:21:00 +0530

    Add error log to check error on live server

--- mailer.php
+++ mailer.php
@@ -9,13 +9,20 @@ require 'PHPMailer/src/SMTP.php';
 function sendEmail($to, $subject, $body) {
     $mail = new PHPMailer(true);
 
+    // Enable full SMTP debug output to error_log
+    $mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {
+        error_log("SMTP Debug level {$level}: {$str}");
+    };
+
     try {
         // Server settings
         $mail->isSMTP();
         $mail->Host       = 'smtp.gmail.com';
         $mail->SMTPAuth   = true;
         $mail->Username   = 'akash.profilics@gmail.com';
-        $mail->Password   = 'pojbqwmqwngvalhw';
+        // $mail->Password   = 'pojbqwmqwngvalhw';
+        $mail->Password   = 'hjwftnvbytezjbof';
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
         $mail->Port       = 465;
         $mail->Timeout    = 30;
@@ -29,10 +36,13 @@ function sendEmail($to, $subject, $body) {
         $mail->Subject = $subject;
         $mail->Body    = $body;
 
+        // Try sending
         $mail->send();
+        error_log("Mail successfully sent to {$to}");
         return true;
     } catch (Exception $e) {
-        error_log("Message could not be sent. Mailer Error: {$mail->ErrorInfo}");
+        error_log("Message could not be sent. PHPMailer Error: " . $mail->ErrorInfo);
+        error_log("Exception message: " . $e->getMessage());
         return false;
     }
 }
