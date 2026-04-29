e7c247080a44bcc308f44afa42f181590c5132dc
yash <yashs.profilics@gmail.com>
2026-04-29 19:04:11 +0530

    fix issue

--- db_connection.php
+++ db_connection.php
@@ -1,11 +1,11 @@
 <?php
 
-$config = require __DIR__ . '/config.php';
+// $config = require __DIR__ . '/config.php';
 
-$host = $config['database']['host'];
-$username = $config['database']['username'];
-$password = $config['database']['password'];
-$database = $config['database']['name'];
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "epic_hrr";
 
 // Create a connection
 $conn = new mysqli($host, $username, $password, $database);
